00d52f5eb646f1de7562a6323e6ce59a656432c3
Toby Mickelson <tmickelson@tinyfern.com>
2026-02-21 01:45:22 -0500

    made more posix compliant, error found that may cause to break on non-macos

--- code/grocery.sh
+++ code/grocery.sh
@@ -18,44 +18,45 @@ then
   exit 1
 fi
 
+echo "WARNING: NEEDS REWRITE, shuf -R ONLY WORKS TESTED ON MACOS"
+
 person=""
-unqueued="$(cat $1 | sort -R | while read name; do printf '%s' "$name" | base64; done)"
-echo "$unqueued"
+unqueued="$(cat "$1" | sort -R | while read -r name; do printf '%s' "$name" | base64 | tr '\n' ' '; done)"
 queued=""
 
 get_name() {
   set -- $unqueued
   person="$1"
   shift
-  unqueued="$@"
+  unqueued="$*"
 }
 
 push_queue() {
-  set -- $queued $person
-  queued="$@"
+  set -- $queued "$person"
+  queued="$*"
 }
 
 pop_queue() {
   set -- $queued
   person="$1"
   shift
-  queued="$@"
+  queued="$*"
 }
 
-while [ -n "$unqueued" -o -n "$queued" ]
+while [ -n "$unqueued" ] || [ -n "$queued" ]
 do
-  if [ $((RANDOM % 2)) -eq 1 -a -n "$unqueued" ]
+  if [ $(($(printf "0\n1\n" | sort -R | head -n 1) % 2)) -eq 1 ] && [ -n "$unqueued" ]
   then
     get_name
-    echo "$(printf '%s' $person | base64 -d) gets in line."
+    echo "$(printf '%s' "$person" | base64 -d) gets in line."
     push_queue
   fi
   sleep 0.15
 
-  if [ $((RANDOM % 2)) -eq 1 -a -n "$queued" ]
+  if [ $(($(printf "0\n1\n" | sort -R | head -n 1) % 2)) -eq 1 ] && [ -n "$queued" ]
   then
     pop_queue
-    echo "$(printf '%s' $person | base64 -d) finishes checking out."
+    echo "$(printf '%s' "$person" | base64 -d) finishes checking out."
   fi
   sleep 0.15
 done
